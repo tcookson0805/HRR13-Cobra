a45ab4c8f83f7827884266e428be37cf5c19a63a
Tyler Cookson <tcookson0805@gmail.com>
2016-02-19 10:28:07 -0800

    filled out functionality on the js pages for my-trip, services, new trip, and trips, and app

--- public/app/app.js
+++ public/app/app.js
@@ -1,23 +1,29 @@
-angular.model('app', ['ui.router']);
-
-app.config(function($stateProvider, $urlRouterProvider) {
-  // For any unmatched url, redirect to /
-  $urlRouterProvider.otherwise("/");
-  // Now set up the states
-  $stateProvider
-    .state('trips', {
-      url: "/trips",
-      templateUrl: "trips.html",
-      controller: "tripsController"
-    })
-    .state('new-trip', {
-      url: "new-trip",
-      templateUrl: "new-trip/new-trip.html",
-      controller: "new-tripController"
-    })
-    .state('my-trip', {
-      url: "my-trip",
-      templateUrl: "my-trip/my-trip.html",
-      controller: "my-tripController"
-    });
-});
+angular.module('tp', [
+    'app.services',
+    'ui.router',
+    'app.my-trip',
+    'app.new-trip',
+    'app.trips',
+    'ngMaterial'
+  ])
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise('/');
+    // Now set up the states
+    $stateProvider
+      .state('trips', {
+        url: '/trips',
+        templateUrl: 'app/trips/trips.html',
+        controller: 'tripsController'
+      })
+      .state('new-trip', {
+        url: '/new-trip',
+        templateUrl: 'app/new-trip/new-trip.html',
+        controller: 'new-tripController'
+      })
+      .state('my-trip', {
+        url: '/my-trip',
+        templateUrl: 'app/my-trip/my-trip.html',
+        controller: 'my-tripController'
+      });
+  });
--- public/app/new-trip/new-trip.js
+++ public/app/new-trip/new-trip.js
@@ -2,14 +2,12 @@ angular.module('app.new-trip', [])
 
 .controller('new-tripController', function($scope, $location){
 
-  $scope.user= {}
-
   $scope.createTrip = function(location, startDate){
     Trips.newTrip(location, startDate)
       .then(function(data){
-        $location.path('/my-trip')
+        $location.path('/my-trip/asdasdasaasadasdadsadasdasdasd')
       })
-  }
-  
+  };
 
-})
+
+});
