d5b6837544f5d6089addbe09649014ab0ca90ee7
Steven Tsao <steventsao713@gmail.com>
2016-02-19 02:07:17 -0800

    Return cookie and related trips upon user login and user/alltrips

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,21 +1,39 @@
 var mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
+var Q = require('q');
 
 var UserSchema = new mongoose.Schema({
   username : {
     type: String,
     required: true,
     unique: true,
-    createdOn: { type: Date, default: Date.now },
   },
   password: {
     type: String,
     required: true,
   },
-  salt: String,
+  // an array of objectID reference to tripSchema
+  trips: [],
+  salt: String
 });
 
 
 
-// build a model from UserSchema
+UserSchema.methods = {
+  generateHash: function(password) {
+    // @method .genSaltSync(9) to encrypt 9 times
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
+  },
+  comparePasswords: function(inputPassword, hash) {
+    // How is salt involved in compareSync?
+    return bcrypt.compareSync(inputPassword, hash);
+  }
+}
+
+// UserSchema.pre('save', function(next){
+//   // this is where we will take the user input and generate the hashed password
+//   // use bcrypt to hash
+// });
+
+
 module.exports = mongoose.model('users', UserSchema);
