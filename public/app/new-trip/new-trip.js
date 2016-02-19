b74c61126f6f0aec38af5a8ff7320b28713251ff
Tyler Cookson <tcookson0805@gmail.com>
2016-02-19 13:44:51 -0800

    created folder and files for authentication

--- public/app/new-trip/new-trip.js
+++ public/app/new-trip/new-trip.js
@@ -1,13 +1,12 @@
 angular.module('app.new-trip', [])
 
-.controller('new-tripController', function($scope, $location){
+.controller('new-tripController', function($scope, $location, Trips) {
 
-  $scope.createTrip = function(location, startDate){
+  $scope.createTrip = function(location, startDate) {
     Trips.newTrip(location, startDate)
-      .then(function(data){
-        $location.path('/my-trip/asdasdasaasadasdadsadasdasdasd')
+      .then(function(data) {
+        $location.path('/my-trip/'+data.tripID)
       })
   };
-
 
 });
