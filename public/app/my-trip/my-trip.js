7d2d5eac964e7ef85064ec74f1134f633f9e8f3e
Tim Dang <timothy.dang@gmail.com>
2016-02-24 15:31:52 -0800

    Added ability to modify trip points of interest.

--- public/app/my-trip/my-trip.js
+++ public/app/my-trip/my-trip.js
@@ -1,21 +1,28 @@
 angular.module('app.my-trip', [])
 
-.controller('my-tripController', function($scope, $location, Trips) {
+.controller('my-tripController', function($scope, $location, Trips, $route) {
 
   //stores information about the current trip
   $scope.thisTrip = {};
-
+  $scope.path = $location.path().substring(9);
   //reqests information about the current trip from the Trips factory
   $scope.getTrip = function() {
-    var path = $location.path().substring(7);
-    $scope.thisTrip = Trips.accessTrip(path);
-  }
+    Trips.accessTrip($scope.path)
+      .then(function(data) {
+        $scope.thisTrip = data;
+      });
+
+  };
   $scope.getTrip();
 
   //sends user edits to the Trips factory
-  $scope.editTrip = function() {
-    Trips.addDetails($scope.thisTrip)
-      .then($scope.getTrip());
-  }
+  $scope.editTrip = function(poi_title, poi_detail) {
+    Trips.addDetails($scope.path, poi_title, poi_detail)
+      .then(function(data) {
+        //reloads the page so you see the new sight you added
+        //this should be changed to something more elegant
+        $route.reload();
+      });
+  };
 
 })
