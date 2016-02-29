7c1d5e3b4405bf3c4c590a49f5ff6e88fa55e8ae
Tyler Cookson <tcookson0805@gmail.com>
2016-02-29 14:14:30 -0800

    insert content into reminder email

--- server/notification_service/mailer.js
+++ server/notification_service/mailer.js
@@ -70,33 +70,34 @@ module.exports = {
     
   },
   
-  // reminderEmail: function(username){
-  //   var options = {
-  //     auth: {
-  //       // api_user:
-  //       // api_key: 
-  //     }
-  //   };
+  reminderEmail: function(username){
+    var auth = {
+      auth: {
+        api_key: 'key-d5f3c75b9660f61d5e0d22d401873be4',
+        domain: 'sandbox838378b8b1a243919bd401f6bbe55d2a.mailgun.org'
+      }
+    }
 
-  //   var mailer = nodemailer.createTransport(sgTransport(options));
+    var nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
-  //   var email = {
-  //     from: '"Marco Polo" <trip.planner.co@gmail.com>',
-  //     to: username,
-  //     subject: 'Reminder: Booking',
-  //     text: 'Get your things booked.',
-  //     html: '<b>Get your things booked.</b>'
-  //   };
-
-  //   console.log('look here', email.to)
-  //   mailer.sendMail(email, function(err, res){
-  //     if (err ){
-  //       console.log(error);
-  //     } else {
-  //       console.log('Message sent: ' , res);
-  //     }
-  //   });
-  // }
+    nodemailerMailgun.sendMail({
+      from: '"Marco Polo" <your.trip.planner.co@sandbox838378b8b1a243919bd401f6bbe55d2a.mailgun.org>',
+      to: username, // An array if you have multiple recipients.
+      subject: 'Booking Reminder!!!',
+      // 'h:Reply-To': 'reply2this@company.com',
+      // //You can use "html:" to send HTML email content. It's magic!
+      html: '<p>Your next trip is just around the corner. This is just a friendly reminder to make sure everything is booked.</p>',
+      //You can use "text:" to send plain-text content. It's oldschool!
+      text: 'Mailgun rocks, pow pow!'
+    }, function (err, info) {
+      if (err) {
+        console.log('Error: ' + err);
+      }
+      else {
+        console.log('Response: ' + info);
+      }
+    });
+  }
   
   // pwdReminder: function(username, password){
   //   var sendPwdReminder = transporter.templateSender(new EmailTemplate('../templates/password_reminder'), {
